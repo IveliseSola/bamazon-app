baff6d638995c382bf1c81c1851827a34a424420
Ivelise Sola <solaivelise@yahoo.com>
2018-03-12 18:23:12 -0400

    db connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,9 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var connection = requirer("./dbConnection.js");
+
+connection.connect(function (err) {
+    if (err) throw err;
+    
+    // functions here
+});
